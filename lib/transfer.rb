7206ed16c40ee9e8d963602a01c28590ca0236c7
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 23:06:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
-  def initialize(sender, receiver, amount, status = "pending"))
+  def initialize(sender, receiver, amount, status = "pending")
     @sender = sender 
     @receiver = receiver
     @amount = amount 
