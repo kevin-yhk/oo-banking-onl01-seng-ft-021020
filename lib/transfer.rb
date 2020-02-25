07e724841986c4a721ec7a78570d32729460df24
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-25 23:19:05 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,9 +20,6 @@ class Transfer
       @sender.balance -= @amount 
       @receiver.balance += @amount
       @status = "complete"
-    else 
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
     end 
   end 
   
